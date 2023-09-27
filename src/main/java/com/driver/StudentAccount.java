f94a3ce217207250f0b89ad5455c0d9e0ff6d3c8
Shivaksk119 <shivapatelkarru119ksk@gmail.com>
2023-09-27 19:42:45 +0530

    2nd attempt

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -2,6 +2,14 @@ package com.driver;
 
 public class StudentAccount extends BankAccount{
 
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
+
     String  institutionName;
 
     public StudentAccount(String name, double balance, String  institutionName) {
